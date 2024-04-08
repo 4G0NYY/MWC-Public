242ce95cb8d7a74345b7b7be68c1c1ecea039a76
4G0NYY <service@agony.ch>
2024-04-08 09:47:33 +0200

    I forgor

--- initial.bat
+++ initial.bat
@@ -1,7 +1,7 @@
 @echo off
 
 echo Installing required packages...
-python -m pip install base58 bip32utils requests pypresence ecdsa pyopencl
+python -m pip install base58 bip32utils requests pypresence ecdsa pyopencl tkinter customtkinter tkterminal
 echo Required packages installed successfully!
 
 echo Install completed.
