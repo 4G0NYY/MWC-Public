561ad7b68d8d43c972a875fe381337249660b435
4G0NYY <service@agony.ch>
2024-08-18 08:30:27 +0200

    Update both GUI and Headless Version

--- gui.py
+++ gui.py
@@ -4,7 +4,9 @@ import binascii
 import requests
 import tkinter
 import customtkinter
+import ctypes
 import socket
+import threading
 import time
 import os
 import subprocess
@@ -14,26 +16,30 @@ import numpy as np
 from multiprocessing import Process
 from pypresence import Presence
 from ecdsa import SigningKey, SECP256k1
+from tkterminal import Terminal
 
 
 
-window_title = 'Militarized Wallet Cracker'
-webhook_url = 'Your_Webhook_URL'    # <-- Change this
 file_path = 'real.txt'
-version = 0.3
+fallback_path = 'EMERGENCY.txt'
+version = 'Version 1'
+
 
 
 def DRPC():
-    client_id = "1199083933955018863"
-    RPC = Presence(client_id)
-    RPC.connect()
-    RPC.update(state="Mining Bitcoin with MWC!" ,
-        start = int(time.time()),
-        large_text="Developed by 4G0NYY",
-        large_image="mew" ,
-        small_image="small2",
-        small_text="hardtruth",
-        buttons=[{"label": "GitHub", "url": "https://github.com/4G0NYY/MWC-Public"}, {"label": "Discord", "url": "https://discord.gg/ZhtcnQsbZz"}])
+    try:
+        client_id = "1199083933955018863"
+        RPC = Presence(client_id)
+        RPC.connect()
+        RPC.update(state="Mining Bitcoin with MWC!" ,
+            start = int(time.time()),
+            large_text="Developed by 4G0NYY",
+            large_image="mew" ,
+            small_image="small2",
+            small_text="hardtruth",
+            buttons=[{"label": "GitHub", "url": "https://github.com/4G0NYY/MWC-Public"}, {"label": "Discord", "url": "https://discord.gg/ZhtcnQsbZz"}])
+    except:
+        switch_var = 0
 
 
 def send_webhook_message(data):
@@ -42,6 +48,13 @@ def send_webhook_message(data):
         print('Webhook message sent successfully.')
     else:
         print('Failed to send webhook message.')
+        print(data) #In case the webhook message fails the data which was supposed to be sent will be printed into the terminal as well as a TXT-File as a kind of "emergency fallback"
+        with open(fallback_path, 'a+') as file:
+            file.write(data)
+
+
+def stoperrormessage():
+    Process.kill(self=all)
 
 
 def import_pubkeys(file_path):
@@ -50,8 +63,8 @@ def import_pubkeys(file_path):
             contents = file.read()
             return contents.decode('utf-8', errors='ignore')
     except FileNotFoundError:
-        print(f"File '{file_path}' not found.")
-        # will have to put an error message in here
+        print(f"File '{file_path}' not found.") 
+        stoperrormessage()
     except Exception as e:
         print(f"An error occurred: {e}")
         return None
@@ -60,92 +73,149 @@ def import_pubkeys(file_path):
 btc = import_pubkeys(file_path)
 
 
+def startMiner3():
+    workerint = workervar.get()
+    for i in range(workerint):
+        miner = Miner()
+        miner.start()
+
+
+class Miner(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self._stop_event = threading.Event()
+
+    def run(self):
+        start = int('000000000000000000000000000000000000000000000002c000000020449e9c', 16)
+        end = int('000000000000000000000000000000000000000000000003ffffffffffffffff', 16)
+        count = 1
+        interval = 3600
+        next_report_time = time.time() + interval
+        try:
+            while True:
+                for i in range(start, end + 1):
+                    puzzleaddr = "btc"
+                    private_key_hex = binascii.hexlify(os.urandom(32)).decode('utf-8')
+                    key_bytes = binascii.unhexlify(private_key_hex)
+                    sk = SigningKey.from_string(key_bytes, curve=SECP256k1)
+                    vk = sk.get_verifying_key()
+                    public_key_bytes = b'\x02' + vk.pubkey.point.x().to_bytes(32, 'big') if vk.pubkey.point.y() % 2 == 0 else b'\x03' + vk.pubkey.point.x().to_bytes(32, 'big')
+                    sha256_hash = hashlib.sha256(public_key_bytes).digest()
+                    ripemd160_hash = hashlib.new('ripemd160', sha256_hash).digest()
+                    extended_ripemd160_hash = b'\x00' + ripemd160_hash
+                    sha256_hash = hashlib.sha256(extended_ripemd160_hash).digest()
+                    sha256_hash = hashlib.sha256(sha256_hash).digest()
+                    checksum = sha256_hash[:4]
+                    binary_address = extended_ripemd160_hash + checksum
+                    address = base58.b58encode(binary_address).decode('utf-8')
+
+                    if address == puzzleaddr:
+                        print(f"WE RICH!! Address: {address}, Hex Key: {private_key_hex}")
+                        with open('hits.txt', 'a') as file:
+                            file.write(f"Address: {address} Hex-Key: {private_key_hex}")
+                        data1 = {
+                            'content': f"WE RICH AF BOYYYY!!\n Address: {address}\n Hex-Key: {private_key_hex}\n"
+                        }
+                        send_webhook_message(data1)
+                        break
+                    else:
+                        print(f"[{count}] Address: {address} | Hex Key: {private_key_hex}")
+
+                    count += 1
+
+                    current_time = time.time()
+                    if current_time >= next_report_time:
+                        ip_address = socket.gethostbyname(socket.gethostname())
+                        data = {
+                            'content': f"Scanned keys: {count}\nStart Key: {hex(start)}\nEnd Key: {hex(end)}\nIP Address: {ip_address}"
+                        }
+                        send_webhook_message(data)
+                        next_report_time = current_time + interval
+        finally:
+            print("Mining has stopped.")
+
+    def get_id(self):
+        if hasattr(self, '_thread_id'):
+            return self._thread_id
+        for id, thread in threading._active.items():
+            if thread is self:
+                return id
+
+    def raise_exception(self):
+        thread_id = self.get_id()
+        res = ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, ctypes.py_object(SystemExit))
+        if res > 1:
+            ctypes.pythonapi.PyThreadState_SetAsyncExc(thread_id, 0)
+            print('Exception raise failure')
+
+def stop1():
+    miner = Miner()
+    miner.stop()
+    miner.join()
+
+
 def stop():
-    exit()
-
-
-def startMiner():
-    workers = workerint.get()
-    with multiprocessing.Pool(processes=workers) as pool:
-        # Map the Miner function to each process
-        freeze_support()
-        pool.map(Miner)
-
-def Miner():
-    count = 1
-    interval = 3600
-    next_report_time = time.time() + interval
-    start = int('000000000000000000000000000000000000000000000002c000000020449e9c', 16)
-    end = int('000000000000000000000000000000000000000000000003ffffffffffffffff', 16)
-
-
-    for i in range(start, end + 1):
-        puzzleaddr = btc
-        private_key_hex = binascii.hexlify(os.urandom(32)).decode('utf-8')
-        key_bytes = binascii.unhexlify(private_key_hex)
-        sk = SigningKey.from_string(key_bytes, curve=SECP256k1)
-        vk = sk.get_verifying_key()
-        public_key_bytes = b'\x02' + vk.pubkey.point.x().to_bytes(32, 'big') if vk.pubkey.point.y() % 2 == 0 else b'\x03' + vk.pubkey.point.x().to_bytes(32, 'big')
-        sha256_hash = hashlib.sha256(public_key_bytes).digest()
-        ripemd160_hash = hashlib.new('ripemd160', sha256_hash).digest()
-        extended_ripemd160_hash = b'\x00' + ripemd160_hash
-        sha256_hash = hashlib.sha256(extended_ripemd160_hash).digest()
-        sha256_hash = hashlib.sha256(sha256_hash).digest()
-        checksum = sha256_hash[:4]
-        binary_address = extended_ripemd160_hash + checksum
-        address = base58.b58encode(binary_address).decode('utf-8')
-
-
-        if address == puzzleaddr:
-            print(f"WE RICH!! Address: {address}, Hex Key: {private_key_hex}")
-            with open('hits.txt', 'a') as file:
-                file.write("Address: {address} Hex-Key: {private_key_hex}")
-            data1 = {
-                'content': f"WE RICH AF BOYYYY!!\n Address: {address}\n Hex-Key: {private_key_hex}\n"
-            }
-            send_webhook_message(data1)
-            break
-        else:
-            print(f"[{count}] Address: {address} | Hex Key: {private_key_hex}")
-
-        count += 1
-
-        current_time = time.time()
-        if current_time >= next_report_time:
-            ip_address = socket.gethostbyname(socket.gethostname())
-            data = {
-                'content': f"Scanned keys: {count}\nStart Key: {hex(start)}\nEnd Key: {hex(end)}\nIP Address: {ip_address}"
-            }
-            send_webhook_message(data)
-            next_report_time = current_time + interval
+    miner = Miner()
+    miner.raise_exception()
+    miner.join()
+    time.sleep(0.5)
 
 
 customtkinter.set_appearance_mode("System")
-customtkinter.set_default_color_theme("blue")
+customtkinter.set_default_color_theme("dark-blue")
+
 
 app = customtkinter.CTk()
 app.geometry("720x480")
 app.title("Militarized Wallet Cracker")
+app.iconbitmap('mwc.ico')
+
 
 title = customtkinter.CTkLabel(app, text="Welcome to the Militarized Wallet Cracker!")
-title.pack(padx=4, pady=10)
+title.pack(padx=4, pady=4)
 
-discord = customtkinter.CTkLabel(app, text="Join my Discord for Help: https://discord.gg/gpt4k7jBbv")
+
+discord = customtkinter.CTkLabel(app, text="Join my Discord for Help: https://discord.agony.ch/")
 discord.pack(padx=4, pady=4)
 
-drcpvar = tkinter.StringVar()
-dunno = customtkinter.CTkEntry(app, width=40, height=40, textvariable=drcpvar)
-dunno.pack()
 
-workerint = tkinter.IntVar()
-dunno2 = customtkinter.CTkEntry(app, width=40, height=40, textvariable=workerint)
-dunno2.pack()
+webhook = customtkinter.CTkLabel(app, text="Input your Webhook-URL and settings below, enjoy!")
+webhook.pack(padx=4, pady=4)
 
 
-idfkanymore = customtkinter.CTkButton(app, text="Start Mining", command=startMiner)
-idfkanymore.pack()
+versonstring = customtkinter.CTkLabel(app, text=version)
+versonstring.pack(padx=4, pady=4)
 
-idfkanymore2 = customtkinter.CTkButton(app, text="Stop Mining", command=stop)
+
+workervar = tkinter.IntVar()
+dunno2 = customtkinter.CTkEntry(app, width=40, height=40, textvariable=workervar)
+dunno2.pack(padx=4, pady=4)
+
+
+webhookvar = tkinter.StringVar()
+aaaa = customtkinter.CTkEntry(app, width=240, height=40, textvariable=webhookvar)
+aaaa.pack(padx=4, pady=4)
+
+
+window_title = 'Militarized Wallet Cracker'
+webhook_url = webhookvar
+file_path = 'real.txt'
+
+
+idfkanymore = customtkinter.CTkButton(app, text="Start Mining", command=startMiner3, fg_color="red")
+idfkanymore.pack(padx=4, pady=4)
+
+
+idfkanymore2 = customtkinter.CTkButton(app, text="Stop Mining", command=stop, fg_color="red")
 idfkanymore2.pack()
 
-app.mainloop()
+
+switch_var = customtkinter.StringVar(value="on")
+switch = customtkinter.CTkSwitch(app, text="Discord Rich Presence", command=DRPC(),
+                                 variable=switch_var, onvalue="on", offvalue="off")
+switch.pack(padx=4, pady=4)
+
+
+if __name__ == '__main__':
+    multiprocessing.freeze_support()
+    app.mainloop()
--- main.py
+++ main.py
@@ -2,48 +2,99 @@ import hashlib
 import base58
 import binascii
 import requests
-import tkinter
-import customtkinter
 import socket
 import time
+import yaml
 import os
+import threading
 #import pyopencl as cl
 import numpy as np
-from multiprocessing import Process
 from pypresence import Presence
 from ecdsa import SigningKey, SECP256k1
 
 
-start = int('000000000000000000000000000000000000000000000002c000000020449e9c', 16)
-end = int('000000000000000000000000000000000000000000000003ffffffffffffffff', 16)
 window_title = 'Militarized Wallet Cracker'
-#webhook_url = input("Your Discord Webhook URL:")    # Not necessary anymore.
+webhook_url = 'YOUR_WEBHOOK_URL'    # <-- Change this
 file_path = 'real.txt'
-version = 0.4
-    
+version = 0.6
 
-def askwebhook():
-    webhook_url = input("Your Discord Webhook URL:")
-    return webhook_url
 
-def DRCP():
-    user_response = input("Do you want to enable the Discord Rich Presence? (y/n): ")
-    if user_response.lower() == 'y':
+
+class DiscordRPC(threading.Thread):
+    def __init__(self):
+       super().__init__()
+       self._stop_event = threading.Event()
+
+    def run(self):
         client_id = "1199083933955018863"
         RPC = Presence(client_id)
         RPC.connect()
-        RPC.update(state="Mining Bitcoin with MWC!" ,
+        RPC.update(state="Mining Crypto with MWC!" ,
             start = int(time.time()),
             large_text="Developed by 4G0NYY",
             large_image="mew" ,
             small_image="small2",
             small_text="hardtruth",
-            buttons=[{"label": "GitHub", "url": "https://github.com/4G0NYY/MWC-Public"}, {"label": "Discord", "url": "https://discord.gg/ZhtcnQsbZz"}])
-    elif user_response.lower() == 'n':
-        print("Okay, Discord Rich Presence is disabled.")
-    else:
-        print("Invalid input. Please enter 'y' for yes or 'n' for no.")
+            buttons=[{"label": "GitHub", "url": "https://github.com/4G0NYY/MWC-Public"}, {"label": "Discord", "url": "https://discord.gg/gpt4k7jBbv"}])
+        while not self._stop_event.is_set():
+            time.sleep(15)
+            continue
+        RPC.close()
 
+    def stop(self):
+        self._stop_event.set()
+
+class Miner(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self._stop_event = threading.Event()
+
+    def run(self):
+        start = int('000000000000000000000000000000000000000000000002c000000020449e9c', 16)
+        end = int('000000000000000000000000000000000000000000000003ffffffffffffffff', 16)
+        count = 1
+        interval = 3600
+        next_report_time = time.time() + interval
+
+        for i in range(start, end + 1):
+            puzzleaddr = btc
+            private_key_hex = binascii.hexlify(os.urandom(32)).decode('utf-8')
+            key_bytes = binascii.unhexlify(private_key_hex)
+            sk = SigningKey.from_string(key_bytes, curve=SECP256k1)
+            vk = sk.get_verifying_key()
+            public_key_bytes = b'\x02' + vk.pubkey.point.x().to_bytes(32, 'big') if vk.pubkey.point.y() % 2 == 0 else b'\x03' + vk.pubkey.point.x().to_bytes(32, 'big')
+            sha256_hash = hashlib.sha256(public_key_bytes).digest()
+            ripemd160_hash = hashlib.new('ripemd160', sha256_hash).digest()
+            extended_ripemd160_hash = b'\x00' + ripemd160_hash
+            sha256_hash = hashlib.sha256(extended_ripemd160_hash).digest()
+            sha256_hash = hashlib.sha256(sha256_hash).digest()
+            checksum = sha256_hash[:4]
+            binary_address = extended_ripemd160_hash + checksum
+            address = base58.b58encode(binary_address).decode('utf-8')
+
+
+            if address == puzzleaddr:
+                print(f"WE RICH!! Address: {address}, Hex Key: {private_key_hex}")
+                with open('hits.txt', 'a') as file:
+                    file.write("Address: {address} Hex-Key: {private_key_hex}")
+                data1 = {
+                    'content': f"WE RICH AF BOYYYY!!\n Address: {address}\n Hex-Key: {private_key_hex}\n"
+                }
+                send_webhook_message(data1)
+                break
+            else:
+                print(f"[{count}] Address: {address} | Hex Key: {private_key_hex}")
+
+            count += 1
+
+            current_time = time.time()
+            if current_time >= next_report_time:
+                ip_address = socket.gethostbyname(socket.gethostname())
+                data = {
+                    'content': f"Scanned keys: {count}\nStart Key: {hex(start)}\nEnd Key: {hex(end)}\nIP Address: {ip_address}"
+                }
+                send_webhook_message(data)
+                next_report_time = current_time + interval
 
 
 def send_webhook_message(data):
@@ -54,6 +105,31 @@ def send_webhook_message(data):
         print('Failed to send webhook message.')
 
 
+def read_yaml():
+    try:
+        with open('config.yaml') as f:
+            data = yaml.load(f, Loader=yaml.FullLoader)
+            use_config = data['USE_CONFIG']
+
+            if use_config == True:
+                    return True
+            else:
+                    return False
+    except:
+        pass
+
+
+def importconfig():
+    with open('config.yaml') as f:
+        data = yaml.load(f,Loader=yaml.FullLoader)
+        address = data['ADDRESS']
+        bad_log = data['BAD_HIT_LOG']
+        webhook = data['DISCORD_WEBHOOK']
+        workers = data['WORKERS']
+
+        return address,workers,bad_log,webhook
+
+
 def import_pubkeys(file_path):
     try:
         with open(file_path, 'rb') as file:
@@ -61,7 +137,7 @@ def import_pubkeys(file_path):
             return contents.decode('utf-8', errors='ignore')
     except FileNotFoundError:
         print(f"File '{file_path}' not found.")
-        # will have to put an error message in here //TODO
+        # will have to put an error message in here
         if True:
             error = 1
             return error
@@ -146,76 +222,27 @@ btc = import_pubkeys(file_path)
 #    cl.enqueue_copy(queue, a, a_buf).wait()
 
 
-def Miner(start, end):
-    count = 1
-    interval = 3600
-    next_report_time = time.time() + interval
-
-#    if os.path.exists('progress.txt'):
-#        with open('progress.txt', 'r') as file:
-#            last_checked_key = int(file.read().strip(), 16)
-#            print(f'Resuming from key: {hex(last_checked_key)}')
-#    else:
-#        last_checked_key = start
-#        print('Starting from the beginning.')
-
-    for i in range(start, end + 1):
-        puzzleaddr = btc
-        private_key_hex = binascii.hexlify(os.urandom(32)).decode('utf-8')
-        key_bytes = binascii.unhexlify(private_key_hex)
-        sk = SigningKey.from_string(key_bytes, curve=SECP256k1)
-        vk = sk.get_verifying_key()
-        public_key_bytes = b'\x02' + vk.pubkey.point.x().to_bytes(32, 'big') if vk.pubkey.point.y() % 2 == 0 else b'\x03' + vk.pubkey.point.x().to_bytes(32, 'big')
-        sha256_hash = hashlib.sha256(public_key_bytes).digest()
-        ripemd160_hash = hashlib.new('ripemd160', sha256_hash).digest()
-        extended_ripemd160_hash = b'\x00' + ripemd160_hash
-        sha256_hash = hashlib.sha256(extended_ripemd160_hash).digest()
-        sha256_hash = hashlib.sha256(sha256_hash).digest()
-        checksum = sha256_hash[:4]
-        binary_address = extended_ripemd160_hash + checksum
-        address = base58.b58encode(binary_address).decode('utf-8')
-
-
-        if address == puzzleaddr:
-            print(f"WE RICH!! Address: {address}, Hex Key: {private_key_hex}")
-            with open('hits.txt', 'a') as file:
-                file.write("Address: {address} Hex-Key: {private_key_hex}")
-            data1 = {
-                'content': f"WE RICH AF BOYYYY!!\n Address: {address}\n Hex-Key: {private_key_hex}\n"
-            }
-            send_webhook_message(data1)
-            break
-        else:
-            print(f"[{count}] Address: {address} | Hex Key: {private_key_hex}")
-
-        count += 1
-
-        current_time = time.time()
-        if current_time >= next_report_time:
-            ip_address = socket.gethostbyname(socket.gethostname())
-            data = {
-                'content': f"Scanned keys: {count}\nStart Key: {hex(start)}\nEnd Key: {hex(end)}\nIP Address: {ip_address}"
-            }
-            send_webhook_message(data)
-            next_report_time = current_time + interval
 
 a = """ 
-        $$\      $$\ $$\      $$\  $$$$$$\  
-        $$$\    $$$ |$$ | $\  $$ |$$  __$$\ 
-        $$$$\  $$$$ |$$ |$$$\ $$ |$$ /  \__|
-        $$\$$\$$ $$ |$$ $$ $$\$$ |$$ |      
-        $$ \$$$  $$ |$$$$  _$$$$ |$$ |      
-        $$ |\$  /$$ |$$$  / \$$$ |$$ |  $$\ 
-        $$ | \_/ $$ |$$  /   \$$ |\$$$$$$  |
-        \__|     \__|\__/     \__| \______/
+     .----------------.  .----------------.  .----------------. 
+    | .--------------. || .--------------. || .--------------. |
+    | | ____    ____ | || | _____  _____ | || |     ______   | |
+    | ||_   \  /   _|| || ||_   _||_   _|| || |   .' ___  |  | |
+    | |  |   \/   |  | || |  | | /\ | |  | || |  / .'   \_|  | |
+    | |  | |\  /| |  | || |  | |/  \| |  | || |  | |         | |
+    | | _| |_\/_| |_ | || |  |   /\   |  | || |  \ `.___.'\  | |
+    | ||_____||_____|| || |  |__/  \__|  | || |   `._____.'  | |
+    | |              | || |              | || |              | |
+    | '--------------' || '--------------' || '--------------' |
+     '----------------'  '----------------'  '----------------' 
 
-            Militarized Wallet Cracker                                                                             
+                      Militarized Wallet Cracker                                                                             
 """
 
 b = """
 Welcome to the Militarized Wallet Cracker!
 This tool is for educational purposes only ;)
-Developed and maintained by 4G0NYY
+This Software is FREE, if you payed for it, you got scammed.
 Join my Discord for Help: https://discord.gg/gpt4k7jBbv
 """
 
@@ -226,17 +253,26 @@ def intro():
 
 
 if __name__ == '__main__':
+
     intro()
-#    askwebhook()
-    webhook_url = input("Your Discord Webhook URL:")
     try:
-        DRCP()
-        num_workers = int(input("How many workers would you like to spawn? "))
-        for i in range(num_workers):
-            p = Process(target=Miner, args=(start, end))
-            p.start()
-    except:
-        num_workers = int(input("How many workers would you like to spawn? "))
-        for i in range(num_workers):
-            p = Process(target=Miner, args=(start, end))
-            p.start()
+        presence_thread = DiscordRPC()
+        presence_thread.start()
+        num_workers = input("How many workers would you like to spawn? ")
+        workerint = int(num_workers)
+
+        for i in range(workerint):
+            miner_thread = Miner()
+            miner_thread.start()
+    except Exception as e:
+        if "Discord" in str(e):
+            num_workers = input("How many workers would you like to spawn? ")
+            workerint = int(num_workers)
+            for i in range(workerint):
+                miner_thread = Miner()
+                miner_thread.start()
+        else:
+            print("Debugger: An Error occurred during code execution. Printing Error...\n")
+            print(e)
+            input("")
+            exit()
