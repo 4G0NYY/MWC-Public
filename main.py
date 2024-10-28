046156358b2065df5dcc616daef289f304ae6ac7
4G0NYY <service@agony.ch>
2024-10-28 12:14:44 +0100

    Update to 1.5 PRE
    Yayyyy

--- main.py
+++ main.py
@@ -9,25 +9,19 @@ import os
 import threading
 import pyopencl as cl
 import siphash24
-#import numpy as np
-#from multiprocessing import Process
 from pypresence import Presence
 from ecdsa import SigningKey, SECP256k1
 
 
-
 window_title = 'Militarized Wallet Cracker'
-webhook_url = 'https://discord.com/api/webhooks/1199070140738838549/b2-lCvbeHL0BhCqkwlVPoWPfvcjRNsMAfQW6tbDOcgXNO_jO23w_4lP85dFps4CtLcjQ'    # <-- Change this
 file_path = 'real.txt'
-version = 0.7
+version = "1.5 PRE"
 
-
-
+# The Discord Process 
 class DiscordRPC(threading.Thread):
     def __init__(self):
        super().__init__()
        self._stop_event = threading.Event()
-
     def run(self):
         client_id = "1199083933955018863"
         RPC = Presence(client_id)
@@ -43,65 +37,58 @@ class DiscordRPC(threading.Thread):
             time.sleep(15)
             continue
         RPC.close()
-
     def stop(self):
         self._stop_event.set()
 
+
+# The Miner Process which does not utilize the GPU
 class Miner(threading.Thread):
-    def __init__(self):
-        super().__init__()
-        self._stop_event = threading.Event()
+    def Miner(start, end):
+        def Miner(a):
+            start = int('000000000000000000000000000000000000000000000002c000000020449e9c', 16)
+            end = int('000000000000000000000000000000000000000000000003ffffffffffffffff', 16)
+            count = 1
+            interval = 3600
+            next_report_time = time.time() + interval
+            for i in range(start, end + 1):
+                puzzleaddr = btc
+                private_key_hex = binascii.hexlify(os.urandom(32)).decode('utf-8')
+                key_bytes = binascii.unhexlify(private_key_hex)
+                sk = SigningKey.from_string(key_bytes, curve=SECP256k1)
+                vk = sk.get_verifying_key()
+                public_key_bytes = b'\x02' + vk.pubkey.point.x().to_bytes(32, 'big') if vk.pubkey.point.y() % 2 == 0 else b'\x03' + vk.pubkey.point.x().to_bytes(32, 'big')
+                sha256_hash = hashlib.sha256(public_key_bytes).digest()
+                ripemd160_hash = hashlib.new('ripemd160', sha256_hash).digest()
+                extended_ripemd160_hash = b'\x00' + ripemd160_hash
+                sha256_hash = hashlib.sha256(extended_ripemd160_hash).digest()
+                sha256_hash = hashlib.sha256(sha256_hash).digest()
+                checksum = sha256_hash[:4]
+                binary_address = extended_ripemd160_hash + checksum
+                address = base58.b58encode(binary_address).decode('utf-8')
+                if address == puzzleaddr:
+                    print(f"WE RICH!! Address: {address}, Hex Key: {private_key_hex}")
+                    with open('hits.txt', 'a') as file:
+                        file.write("Address: {address} Hex-Key: {private_key_hex}")
+                    data1 = {
+                        'content': f"WE RICH AF BOYYYY!!\n Address: {address}\n Hex-Key: {private_key_hex}\n"
+                    }
+                    send_webhook_message(data1)
+                    break
+                else:
+                    print(f"[{count}] Address: {address} | Hex Key: {private_key_hex}")
+                count += 1
+                current_time = time.time()
+                if current_time >= next_report_time:
+                    ip_address = socket.gethostbyname(socket.gethostname())
+                    data = {
+                        'content': f"Scanned keys: {count}\nStart Key: {hex(start)}\nEnd Key: {hex(end)}\nIP Address: {ip_address}"
+                    }
+                    send_webhook_message(data)
+                    next_report_time = current_time + interval
+    def stop(self):
+        self._stop_event.set()
 
-    def run(self):
-        start = int('000000000000000000000000000000000000000000000002c000000020449e9c', 16)
-        end = int('000000000000000000000000000000000000000000000003ffffffffffffffff', 16)
-        count = 1
-        interval = 3600
-        next_report_time = time.time() + interval
-
-        for i in range(start, end + 1):
-            puzzleaddr = btc
-            private_key_hex = binascii.hexlify(os.urandom(32)).decode('utf-8')
-            key_bytes = binascii.unhexlify(private_key_hex)
-            sk = SigningKey.from_string(key_bytes, curve=SECP256k1)
-            vk = sk.get_verifying_key()
-            public_key_bytes = b'\x02' + vk.pubkey.point.x().to_bytes(32, 'big') if vk.pubkey.point.y() % 2 == 0 else b'\x03' + vk.pubkey.point.x().to_bytes(32, 'big')
-            sha256_hash = hashlib.sha256(public_key_bytes).digest()
-            ripemd160_hash = hashlib.new('ripemd160', sha256_hash).digest()
-            extended_ripemd160_hash = b'\x00' + ripemd160_hash
-            sha256_hash = hashlib.sha256(extended_ripemd160_hash).digest()
-            sha256_hash = hashlib.sha256(sha256_hash).digest()
-            checksum = sha256_hash[:4]
-            binary_address = extended_ripemd160_hash + checksum
-            address = base58.b58encode(binary_address).decode('utf-8')
-
-
-            if address == puzzleaddr:
-                print(f"WE RICH!! Address: {address}, Hex Key: {private_key_hex}")
-                with open('hits.txt', 'a') as file:
-                    file.write("Address: {address} Hex-Key: {private_key_hex}")
-                data1 = {
-                    'content': f"WE RICH AF BOYYYY!!\n Address: {address}\n Hex-Key: {private_key_hex}\n"
-                }
-                send_webhook_message(data1)
-                break
-            else:
-                print(f"[{count}] Address: {address} | Hex Key: {private_key_hex}")
-
-            count += 1
-
-            current_time = time.time()
-            if current_time >= next_report_time:
-                ip_address = socket.gethostbyname(socket.gethostname())
-                data = {
-                    'content': f"Scanned keys: {count}\nStart Key: {hex(start)}\nEnd Key: {hex(end)}\nIP Address: {ip_address}"
-                }
-                send_webhook_message(data)
-                next_report_time = current_time + interval
-
-
-
-
+# The Process to send the message to the webhook
 def send_webhook_message(data):
     response = requests.post(webhook_url, json=data)
     if response.status_code == 204:
@@ -109,13 +96,12 @@ def send_webhook_message(data):
     else:
         print('Failed to send webhook message.')
 
-
+# Read the YAML File
 def read_yaml():
     try:
         with open('config.yaml') as f:
             data = yaml.load(f, Loader=yaml.FullLoader)
             use_config = data['USE_CONFIG']
-
             if use_config == True:
                     return True
             else:
@@ -123,36 +109,54 @@ def read_yaml():
     except:
         pass
 
-
-def importconfig():
+## Import all the Configs from the YAML
+def importconfigyes():
     with open('config.yaml') as f:
         data = yaml.load(f,Loader=yaml.FullLoader)
-        address = data['ADDRESS']
-        bad_log = data['BAD_HIT_LOG']
+        useconfig = data['USE_CONFIG']
+        return useconfig
+
+
+def importconfiggpu():
+    with open('config.yaml') as f:
+        data = yaml.load(f,Loader=yaml.FullLoader)
+        gpufast = data['GPU_ACCELERATION']
+        return gpufast
+
+
+def importconfigwebhook():
+    with open('config.yaml') as f:
+        data = yaml.load(f,Loader=yaml.FullLoader)
         webhook = data['DISCORD_WEBHOOK']
-        workers = data['WORKERS']
-
-        return address,workers,bad_log,webhook
+        return webhook
 
 
+def importconfigworkers():
+    with open('config.yaml') as f:
+        data = yaml.load(f,Loader=yaml.FullLoader)
+        num_workers = data['WORKERS']
+        return num_workers
+## End of import-config-section 
+
+# Import the real.txt File with all the public keys and load them into the RAM
 def import_pubkeys(file_path):
     try:
         with open(file_path, 'rb') as file:
             contents = file.read()
             return contents.decode('utf-8', errors='ignore')
     except FileNotFoundError:
-        print(f"File '{file_path}' not found.")
-        print("The file with all the addresses cannot be found. Did you place it in the correct directory?")
+        print(f"Debugger: File '{file_path}' not found.")
+        print("Press Enter to exit")
         if True:
-            error = 1
-            return error
+            input("")
+            exit()
     except Exception as e:
         print(f"An error occurred: {e}")
-        return None
-    return error
 
+# Assign the now loaded publickeys to the variable "btc"
 btc = import_pubkeys(file_path)
 
+# The Miner Process which does utilize the GPU
 class MinerGPU():
     def MinerGPU(start, end):
         def Miner_GPU(a):
@@ -219,67 +223,75 @@ class MinerGPU():
             prg.add(queue, a.shape, None, a_buf)
             cl.enqueue_copy(queue, a, a_buf).wait()
 
-
-
-a = """ 
-     .----------------.  .----------------.  .----------------. 
-    | .--------------. || .--------------. || .--------------. |
-    | | ____    ____ | || | _____  _____ | || |     ______   | |
-    | ||_   \  /   _|| || ||_   _||_   _|| || |   .' ___  |  | |
-    | |  |   \/   |  | || |  | | /\ | |  | || |  / .'   \_|  | |
-    | |  | |\  /| |  | || |  | |/  \| |  | || |  | |         | |
-    | | _| |_\/_| |_ | || |  |   /\   |  | || |  \ `.___.'\  | |
-    | ||_____||_____|| || |  |__/  \__|  | || |   `._____.'  | |
-    | |              | || |              | || |              | |
-    | '--------------' || '--------------' || '--------------' |
-     '----------------'  '----------------'  '----------------' 
-
-                      Militarized Wallet Cracker                                                                             
-"""
-
+# Idk why I made this variable
+# But yes
 b = """
 Welcome to the Militarized Wallet Cracker!
 This tool is for educational purposes only ;)
 This Software is FREE, if you payed for it, you got scammed.
 Join my Discord for Help: https://discord.gg/gpt4k7jBbv
 """
-
+# Kind of unnecessary but oh welllll
 def intro():
     print(version)
-    print(a)
     print(b)
 
 
 if __name__ == '__main__':
 
     intro()
-    MinuteGPU = input("Do you wish to use GPU-Accelerated Computing? (y/n)")
-    if MinuteGPU == "y":
-        try:
-            presence_thread = DiscordRPC()
-            presence_thread.start()
-            num_workers = input("How many workers would you like to spawn?")
-            workerint = int(num_workers)
-
-            for i in range(workerint):
-                MinerGPU.thread = MinerGPU()
-                MinerGPU.thread.start()
-        except Exception as e:
-            if "Discord" in str(e):
+    useconfig = importconfigyes()
+    if useconfig == 'True':     # If the Config File should be used
+        webhook_url = importconfigwebhook()
+        num_workers = importconfigworkers()
+        gpufast = importconfiggpu()
+        if gpufast == "True":       # if GPU-Acceleration is enabled
+            try:
+                presence_thread = DiscordRPC()
+                presence_thread.start()
+                workerint = int(num_workers)
                 for i in range(workerint):
-                    MinerGPU.thread = MinerGPU()
-                    MinerGPU.start()
+                    Miner_thread = MinerGPU()
+                    Miner_thread.start()
+            except Exception as e:
+                if "Discord" in str(e):
+                    for i in range(workerint):
+                        Miner.thread = Miner()
+                        Miner.thread.start()
+                else:
+                    print("Debugger: An Error occurred during code execution. Printing Error...\n")
+                    print(e)
+                    input("")
+                    exit()
+        elif gpufast == 'False':    # if GPU-Acceleration is disabled
+            try:
+                presence_thread = DiscordRPC()
+                presence_thread.start()
+                workerint = int(num_workers)
+                for i in range(workerint):
+                    miner_thread = Miner()
+                    miner_thread.start()
+            except Exception as e:
+                if "Discord" in str(e):
+                    for i in range(workerint):
+                        Miner.thread = Miner()
+                        Miner.thread.start()
+                else:
+                    print("Debugger: An Error occurred during code execution. Printing Error...\n")
+                    print(e)
+                    input("")
+                    exit()
         else:
-            print("Debugger: An Error occurred during code execution. Printing Error...\n")
+            print("Debugger: No GPU-Acceleration value is defined in the Configuration File (config.yaml)")
             input("")
             exit()
-    else:
+    elif useconfig == 'False':
         try:
             presence_thread = DiscordRPC()
             presence_thread.start()
             num_workers = input("How many workers would you like to spawn? ")
             workerint = int(num_workers)
-
+            webhook_url = input("What's your Discord Webhook URL?")
             for i in range(workerint):
                 miner_thread = Miner()
                 miner_thread.start()
@@ -295,3 +307,9 @@ if __name__ == '__main__':
                 print(e)
                 input("")
                 exit()
+    else:
+        print("Debugger: There's an error in your config.yaml")
+        print("Debugger: Check if you have it in the same formatting as the configtemplate")
+        print("Press Enter to exit.")
+        input("")
+        exit()
